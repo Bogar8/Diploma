6fc6a2e386954f4a47afa5c13a0668b4d23cb552
Bogar8 <62038397+Bogar8@users.noreply.github.com>
2022-05-19 19:49:49 +0200

    Database connect

--- src/main/kotlin/com/example/diplomska/app/Main.kt
+++ src/main/kotlin/com/example/diplomska/app/Main.kt
@@ -3,33 +3,21 @@ package com.example.diplomska.app
 import com.example.diplomska.model.Category
 import com.example.diplomska.model.Product
 import com.example.diplomska.util.DatabaseUtil
-import com.mongodb.client.ClientSession
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+import com.example.diplomska.util.DocumentUtil
 import model.ProductStock
-import org.bson.Document
 import org.litote.kmongo.eq
 import org.litote.kmongo.findOne
 
 
-import org.litote.kmongo.findOneById
-import org.litote.kmongo.json
 import java.time.LocalDateTime
 import java.util.*
 import kotlin.collections.ArrayList
 
 
-
 fun main() {
     val database = DatabaseUtil.getConnection()
     println("SANDBOX")
-    val p = ProductStock(null, 10, 20.2)
-    val format = Json { prettyPrint = true }
-    var json = format.encodeToString(p)
-    println(json)
-    val json2 = format.decodeFromString<ProductStock>(json)
-    println(json2.toString())
+
 
     val purchaseHistory: ArrayList<ProductStock> = arrayListOf(
         ProductStock(null, 10, 20.99, LocalDateTime.of(2019, 1, 1, 12, 0)),
@@ -45,23 +33,27 @@ fun main() {
         ProductStock(null, 5, 18.99, LocalDateTime.of(2020, 1, 1, 12, 0)),
         ProductStock(null, 1, 101.99, LocalDateTime.of(2020, 1, 2, 12, 0))
     )
-    val testSample: Product =
-        Product(UUID.randomUUID().toString(),12345, "name", Category.FOOD, 0, null, true, sellingHistory, purchaseHistory)
+    val testSample =
+        Product(
+            UUID.randomUUID().toString(),
+            12345,
+            "name",
+            Category.FOOD,
+            0,
+            null,
+            true,
+            sellingHistory,
+            purchaseHistory
+        )
 
 
     val collection = database.getCollection(Product.DATABASE_NAME)
-    collection.insertOne(Document.parse(Json.encodeToString(testSample)))
-    val result = collection.findOneById(testSample._id)?.toJson()
-    var list = ArrayList<Product>()
-    println(collection.find().forEach{
-        list.add(Json.decodeFromString(it.json))
-    })
-    println(list)
-    println(list.size)
+    collection.insertOne(DocumentUtil.encode(testSample))
 
-    if (result != null) {
-        println(Json.decodeFromString<Product>(result).toString())
+    val answer = collection.findOne(Product::_id eq testSample._id)
+
+
+    if (answer != null) {
+        println(DocumentUtil.decode<Product>(answer).toString())
     }
-
-
 }
