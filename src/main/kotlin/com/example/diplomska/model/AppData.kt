a37aa0238619664595c8d31a77cb69c7ab0664dd
Bogar8 <62038397+Bogar8@users.noreply.github.com>
2022-05-30 20:44:44 +0200

    changes

--- src/main/kotlin/com/example/diplomska/model/AppData.kt
+++ src/main/kotlin/com/example/diplomska/model/AppData.kt
@@ -2,12 +2,14 @@ package com.example.diplomska.model
 
 
 class AppData(
-    var companyInformation: CompanyInformation,
-    var products: ArrayList<Product> = ArrayList(),
-    var employees: ArrayList<User> = ArrayList(),
-    var invoices: ArrayList<Invoice> = ArrayList(),
-    var loggedInUser: User?
 ) {
+    companion object{
+        var companyInformation: CompanyInformation? = null
+        var products: ArrayList<Product> = ArrayList()
+        var employees: ArrayList<User> = ArrayList()
+        var invoices: ArrayList<Invoice> = ArrayList()
+        var loggedInUser: User? = null
+    }
 
     override fun toString(): String {
         return "$companyInformation\nproducts:$products\nemployees:$employees\n" +
